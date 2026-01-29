c6abb7d1f67454b1aea3227dd5a43b650ba42fe9
princesmkdt <24tkj1-37@smkdata.sch.id>
2026-01-29 13:56:11 +0700

    gtw

--- ujikom-debian.sh
+++ ujikom-debian.sh
@@ -1,10 +1,14 @@
-echo "MODUL UJIKOM DEBIAN 11"
-echo "auto eth0" >> /etc/network/interfaces
-echo "iface eth0 inet dhcp"
-nano /etc/network/interfaces
-systemctl restart networking
-nano /etc/resolv.conf | echo "nameserver 192.168.100.x nameserver 192.168.100.1 nameserver 8.8.8.8"
-chattr +i /etc/resolv.conf
-echo "anda harus ping google dulu"
+
 echo "http://deb.debian.org/debian bullseye main contrib non-free"
 apt update 
+sudo apt install bind9 dnsutils -y 
+nano /etc/bind/named.conf.local | echo 
+'zone “tkjsmkdt.org”{
+file “/etc/bind/conf_domain”;
+Type master;
+};
+
+zone “100.168.192.in-addr.arpa” {
+type master;
+file “/etc/bind/conf_ip”;
+};'
